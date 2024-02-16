9327d79412e206028a12caf7edffd7267610738a
baldockb2514 <111828862+baldockb2514@users.noreply.github.com>
2024-02-16 13:29:27 +1300

    Prints Instructions

---
+++ C_11_show_instructions.py
@@ -0,0 +1,21 @@
+# Shows instructions
+def show_instructions():
+    print('''\n 
+***** Instructions *****
+ 
+For each ticket, enter ...
+- The person's name (can't be blank)
+- Age (between 12 and 120)
+- Payment Method (cash / credit
+ 
+When you have entered all the users, press 'xxx' to quit.
+ 
+The program will then display all the ticket details
+including the cost of each ticket, the total cos
+and the total profit.
+ 
+This information will also be automatically written to a text file
+ 
+**************************''')
+
+show_instructions()
